1598d116bc31007e7af17d42f490ffd38d2d6676
Informatica Electoral <soporte@informaticaelectoral.com>
2023-10-24 11:43:23 -0700

    add modelo data

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 from fastapi import FastAPI, UploadFile, Form, File, Response
-
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class DatosUsuario(BaseModel):
+    name: str
+    olds: str
+    password : str
+    confi_password: str
+    
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8200"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root(response: Response = Response()):
     return {"message": "403 Forbidden"}
 
-@app.post("registrar-usuario")
-async def root(name: str = Form(),
-               gmail: str = Form(),
-               password: str = Form(),
-               confi_password: str = Form()):
+@app.post("/registrar-usuario")
+async def registrar_usuario(data: DatosUsuario):
+    if data.password == data.confi_password:
+        print('contraseñas igual.....')
     
-    print('name: ',name)
     return {"message": "403 Forbidden"}
